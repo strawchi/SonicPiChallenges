f114ab274bd1886fbe2e2d261593034e22e648bb
strawchi <chiamaka_igwebuike@student.davincischools.org>
2026-04-10 10:57:08 -0700

    Adjust audio samples and volumes in the script

--- This_Is_How_We_Do_It.rb
+++ This_Is_How_We_Do_It.rb
@@ -1,6 +1,7 @@
 # This Is How We Do It
 use_bpm 104
 use_synth :saw
+chatter = "C:/Users/chiamaka_igwebuike/Documents/Audacity/chatting.wav"
 start = "C:/Users/chiamaka_igwebuike/Documents/Audacity/do_itt.wav"
 scat = "C:/Users/chiamaka_igwebuike/Documents/Audacity/scating.wav"
 chorus = "C:/Users/chiamaka_igwebuike/Documents/Audacity/This Is How We Do It.wav"
@@ -8,6 +9,8 @@ chorus = "C:/Users/chiamaka_igwebuike/Documents/Audacity/This Is How We Do It.wa
 melody = [:a3,:fb3,:a3,:a3,:fb3,:a3,:a3,:ab3,:a3,:a3,:a3]
 melSleeps = [1, 0.5, 1, 0.5, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 1.5]
 i = 0
+sample chatter, amp: 1.5
+sleep 14.5
 
 define :melod do |vol|
   11.times do
@@ -19,7 +22,7 @@ define :melod do |vol|
     end
   end
 end
-sample start
+sample start,amp: 1.5
 sleep 4
 
 live_loop :snare do
@@ -45,11 +48,11 @@ live_loop :main do
   stop
 end
 sleep 3.8
-sample chorus
+sample chorus,amp: 1.5
 sleep 3
-sample scat, amp: 0.5
+sample scat
 sleep 5
-sample chorus
+sample chorus,amp: 1.5
 sleep 4.2
 volume = 1
 live_loop :ending do
